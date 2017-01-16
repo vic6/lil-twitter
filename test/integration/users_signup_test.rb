5b6e4c2367dc447c4e442a88ee10d9b8af9f308d
vic6 <vicmatthews116@gmail.com>
2017-01-15 22:32:03 -0800

    Add flash params test, finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -13,5 +12,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
+  end
+
+  test 'valid signup' do
+    assert_difference "User.count" do
+      post users_path, params: { user: { name: "Dracula",
+                                         email: "dracula@dog.com",
+                                         password: "dracrulez",
+                                         password_confirmation: "dracrulez" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.empty?
   end
 end
