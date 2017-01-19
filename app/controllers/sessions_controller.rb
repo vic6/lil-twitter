2bf254469bde66d8d89fcb93821f5e6e958e6f73
vic6 <vicmatthews116@gmail.com>
2017-01-18 21:45:43 -0800

    Add user login helper methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # go to user show page
+      log_in(user)
+      redirect_to(user)
     else
       # flash.now dissapears as soon as there is an additional request
       flash.now[:danger] = 'Incorrect email/password: Self-destruct initiated'
