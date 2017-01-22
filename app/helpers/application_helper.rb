3dddb74bec6c5c018e8f86d4cf8981cec8b5915c
vic6 <vicmatthews116@gmail.com>
2017-01-22 12:17:08 -0800

    Change titles in static pages and layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   #Returns the full titls on a per-page basis
   def full_title(page_title = '')
-    base_title = "Ruby on Rails Tutorial Sample App"
+    base_title = "Lil' Twitter"
     if page_title.empty?
       base_title
     else
