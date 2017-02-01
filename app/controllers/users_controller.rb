cd3e1954c1c99e4973d84b95aeefa19d99403cff
vic6 <vicmatthews116@gmail.com>
2017-01-31 18:29:21 -0800

    Add delete action to users controller, prevent unautorized users from having access to delete action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  # prevents unauthorized users to delete users from command line
+  before_action :admin_user, only: :destroy
 
   def index
     @users = User.paginate(page: params[:page])
@@ -40,8 +42,9 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    log_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
@@ -63,5 +66,10 @@ class UsersController < ApplicationController
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
+  end
+
+  # Confirms admin user
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 end
