f969724455d7b217f20bd85bdc6c1e1c73682f8d
vic6 <vicmatthews116@gmail.com>
2017-01-31 18:42:24 -0800

    Add test admin to user.yml, add user controller test checking for proper rerouting of unauthorized users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -36,6 +36,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test "should redirect destroy when not logged in" do
+    assert_no_difference "User.count" do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference "User.count" do
+      delete user_path(@user)
+    end
+    assert_redirected_to root_url
+  end
   # test not yet implemented
   # test "should not allow the admin attribute to be edited via the wef" do
   #   log_in_as(@other_user)
