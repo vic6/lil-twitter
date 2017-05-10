98a3fd57bb459f24637c09b4a1353d44d147e98f
vic6 <vicmatthews116@gmail.com>
2017-05-10 14:48:49 -0700

    Add user profile integration test, fix typo in application helpter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  #Returns the full titls on a per-page basis
+  #Returns the full title on a per-page basis
   def full_title(page_title = '')
     base_title = "Lil' Twitter"
     if page_title.empty?
