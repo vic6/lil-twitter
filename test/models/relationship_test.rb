0c12ffc7c3b2e2d8eeb7617a3d8da200c69b982b
vic6 <vicmatthews116@gmail.com>
2017-05-13 14:52:03 -0700

    Add relationship model tests

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class RelationshipTest < ActiveSupport::TestCase
-  # def setup
-  #   @relationship = Relationship.new(follower_id: users(:victor).id,
-  #                                    followed_id: users(:smitty).id)
-  # end
+  def setup
+    @relationship = Relationship.new(follower_id: users(:victor).id,
+                                     followed_id: users(:smitty).id)
+  end
+
+  test 'should be vailid' do
+    assert @relationship.valid?
+  end
+
+  test 'shold require a follower' do
+    assert @relationship.follower_id == users(:victor).id
+  end
+
+  test 'should require someone to be followd' do
+    assert @relationship.followed_id == users(:smitty).id
+  end
 end
