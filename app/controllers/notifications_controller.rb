93367da799415abc02e56317e32b9c63263258c6
vic6 <vicmatthews116@gmail.com>
2017-05-25 17:06:38 -0700

    Text notification tells admin when new user signs up

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,8 +5,8 @@ class NotificationsController < ApplicationController
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
     @client.account.messages.create(from: @twilio_number,
-                                    to: '7028398971',
-                                    body: 'Time for some Ranch',
+                                    to: @my_number,
+                                    body: current_user.name + ' just signed up!',
                                     media_url: 'http://i.imgur.com/NQqAhsK.jpg')
     redirect_to user_path(current_user)
   end
