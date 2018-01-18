a5690bed0381a86e22d28da9e4f238a7c64cf265
vic6 <vicmatthews116@gmail.com>
2018-01-18 15:27:59 -0800

    add account back into api request

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -4,10 +4,10 @@ class NotificationsController < ApplicationController
     @my_number = ENV['PHONE_NUMBER']
     @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
 
-    @client.messages.create(from: @twilio_number,
-                            to: @my_number,
-                            body: current_user.name + ' just signed up!',
-                            media_url: 'https://vignette.wikia.nocookie.net/spongebob/images/8/8e/Spongebob-rainbow-meme-990x557.jpg/revision/latest/scale-to-width-down/640?cb=20170731020011')
+    @client.account.messages.create(from: @twilio_number,
+                                    to: @my_number,
+                                    body: current_user.name + ' just signed up!',
+                                    media_url: 'https://vignette.wikia.nocookie.net/spongebob/images/8/8e/Spongebob-rainbow-meme-990x557.jpg/revision/latest/scale-to-width-down/640?cb=20170731020011')
     redirect_to user_path(current_user)
   end
 
